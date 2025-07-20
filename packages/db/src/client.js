48cd3720a626febcb9ead08963c00444274ff4aa
Abhishek <Abhishekraut8697@gmail.com>
2025-07-20 20:03:44 +0530

    cleanup db

--- packages/db/src/client.js
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-// Remove the extension-accelerate import and usage due to missing module/type
-// Use globalThis for compatibility in Node.js environments
-const globalForPrisma = globalThis;
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-//# sourceMappingURL=client.js.map
